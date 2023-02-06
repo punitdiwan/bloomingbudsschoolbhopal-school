d72e7e9fa3b58ede344ee446ece8724aed42de50
narendra <you@example.com>
2023-02-06 16:49:39 +0530

    third commit

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,47 +1,38 @@
-import React from 'react'
-import './css/Navbar.css'
-
+import React from "react";
+import "./css/Navbar.css";
 
 const Navbar = () => {
   return (
     <>
-    <div className="container-fluid navmain">
-       <div className="navleft">
-         <div className="logo">
+      <div className="container-fluid navmain">
+        <div className="navleft">
+          <div className="logo">
             <img src="./images/Blooming_Logo_1.png" alt="" />
-         </div>
-       </div>
-       <div className="navmid">
-           <h1><span id='firsth'>B</span>LOOMING <span id="firsth">B</span>UDS  <span id="firsth">P</span>LAY <span id="firsth">S</span>CHOOL</h1>
-           <p>Learning today. Leading tomorrow. Inspired Forever.</p>
-       </div>
-       <div className="navright">
-            <div className="socialmedia">
-            <a href="https://www.facebook.com/Hogwarts1234" target="_blank"style={{textDecoration:"none",color:"white"}}> <i className="ri-facebook-fill"></i></a>
-
+          </div>
+        </div>
+        <div className="navmid">
+          <h1>
+            <span id="firsth">B</span>LOOMING <span id="firsth">B</span>UDS{" "}
+            <span id="firsth">P</span>LAY <span id="firsth">S</span>CHOOL
+          </h1>
+          <p>Learning today. Leading tomorrow. Inspired Forever.</p>
+        </div>
+        <div className="navright">
+          <div className="socialmedia">
+            <i className="ri-facebook-fill"></i>
             <i className="ri-linkedin-box-fill"></i>
             <i className="ri-youtube-line"></i>
+          </div>
+          <div className="seacrch">
+            <input className="aa" type="search" />
+            <div className="btn" style={{ backgroundColor: "#F8D34D" }}>
+              Search
             </div>
-            <div className="seacrch">
-                <input className='aa' type="search" />
-                <div className="btn" style={{backgroundColor:"#F8D34D"}}>Search</div>
-            </div>
-           
-
-       </div>
-
-
-
-
-
-    </div>
-    
-    
-    
-    
-    
+          </div>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/Components/Updates.js
+++ src/Components/Updates.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Updates = () => {
   return (
   <>
-  <div className="updates">
+  {/* <div className="updates">
         <div className="updatesleft">
             <div className="innerup">
                 Updates
@@ -14,12 +14,12 @@ const Updates = () => {
             <a href=""><p>1. Admission will open for 2023</p></a>
             <a href=""><p>2. School Transport Arrangement</p></a>
             </div>
-        {/* <div className="updatesright">
+        <div className="updatesright">
     
-        </div> */}
+        </div>
 
 
-</div>
+</div> */}
   </>
   )
 }
